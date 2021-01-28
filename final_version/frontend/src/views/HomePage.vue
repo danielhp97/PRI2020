71dd794a4370f49a559e0e23dccd03b93b4c4f8c
GuiM117 <36679610+GuiM117@users.noreply.github.com>
2021-01-28 12:12:39 +0000

    Estrutura base do Vue
    Adicionando rotas e views, pode substituir a /app

---
+++ final_version/frontend/src/views/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <v-app>
+      <NavBar></NavBar>
+      <h1> Hello world</h1>
+    </v-app>
+
+   
+</template>
+
+<script>
+
+import NavBar from "@/components/NavBar";
+
+export default {
+    name: "HomePage",
+  components: {
+    NavBar
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
